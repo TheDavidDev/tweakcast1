af442d4833071732fb41c54d086ffcc292b73aab
kiranjcdev <81453897+kiranjcdev@users.noreply.github.com>
2021-03-27 14:16:24 +0530

    Add files via upload

---
+++ public/store.js
@@ -0,0 +1,160 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    const data = document.querySelector('.btn-purchase').getAttribute('data-order')
+    fetch('http://localhost:3000/payment', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: data,
+    })
+        .then(response => response.json())
+        .then(data => {
+            var cartItems = document.getElementsByClassName('cart-items')[0]
+            while (cartItems.hasChildNodes()) {
+                cartItems.removeChild(cartItems.firstChild)
+            }
+            updateCartTotal()
+            window.open(`http://localhost:3000/payment/${data.order_id}`);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}  
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+function modifyOrderPayload (orderData) {
+    const modifiedItems = orderData.orderItems.map(item => {
+        return {
+            "type": "physical",
+            "reference": item.itemName,
+            "name": item.itemName,
+            "quantity": item.quantity,
+            "quantity_unit": "pcs",
+            "unit_price": item.price * 100,
+            "tax_rate": 0,
+            "total_amount": (item.quantity * item.price) * 100,
+            "total_discount_amount": 0,
+            "total_tax_amount": 0
+        }
+    })
+    const payload = {
+        "purchase_country": "GB",
+        "purchase_currency": "GBP",
+        "locale": "en-GB",
+        "order_amount": orderData.total * 100,
+        "order_tax_amount": 0,
+        "order_lines": modifiedItems,
+        "merchant_urls": {
+            "terms": "https://www.example.com/terms.html",
+            "checkout": "https://www.example.com/checkout.html",
+            "confirmation": "http://localhost:3000/confirmation?klarna_order_id={checkout.order.id}",
+            "push": "https://www.example.com/api/push"
+        }
+    }
+    document.querySelector('.btn-purchase').setAttribute('data-order' , JSON.stringify(payload));
+
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    const orderData = {total: 0, orderItems: []}
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var itemName = cartRow.getElementsByClassName('cart-item-title')[0].innerHTML
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = parseFloat(quantityElement.value)
+        total = total + (price * quantity)
+        orderData.orderItems.push({
+            itemName,
+            quantity,
+            price
+        })        
+    }
+    total = Math.round(total * 100) / 100
+    orderData.total = total
+    modifyOrderPayload (orderData)
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const axios = require('axios');
+const { response } = require('express');
+
+require('dotenv').config()
+const corsOptions = {
+    exposedHeaders: 'Authorization',
+  };
+app.use(cors(corsOptions));
+app.use(bodyParser.json())
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+let token = '';
+const localDB = []
+app.get('/payment/:order_id', (req, res) => {
+    if (localDB.length) {
+        const paymentData = localDB.find(order => order.order_id === req.params.order_id)
+        if (Object.keys(paymentData).length) {
+            res.render('payment', { data: paymentData.html_snippet })
+        } else {
+            res.send({ status: "error" })
+        }
+    } else {
+        res.send({ status: "error" })
+    }
+});
+
+app.get('/confirmation', (req, res) => {
+
+    axios.get(`${process.env.KLARNA_API_ENDPOINT}/checkout/v3/orders/${req.query.klarna_order_id}`,{
+        headers: {
+          Authorization: token
+        }
+      })
+        .then(function (response) {
+            res.render('confirmation', {data: response.data.html_snippet})
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.send({ status: "error" })
+        }); 
+})
+
+app.post('/payment', (req, res) => {
+    axios.post(`${process.env.KLARNA_API_ENDPOINT}/checkout/v3/orders`, req.body, {
+        headers: { 'content-type': 'application/json' },
+        auth: {
+            username: process.env.KLARNA_USERNAME,
+            password: process.env.KLARNA_PASSWORD,
+        }
+    })
+        .then(function (response) {
+            localDB.push(response.data)
+            token = 'Basic' + response['request']['_header'].split("Basic")[1].split("==")[0] + '=='
+            res.send({ status: "success", order_id: response.data.order_id })
+        })
+        .catch(function (error) {
+            res.send({ status: "error" })
+        }); 
+});
+
+
+app.listen(3000, () => console.log('listening at 3000'));
