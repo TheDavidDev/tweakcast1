d32a4ce6359532fd5178337048b98c8cb206bb6f
kiranjcdev <81453897+kiranjcdev@users.noreply.github.com>
2021-03-28 09:20:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const axios = require('axios');
 const { response } = require('express');
+const fs = require("fs");
+const orders = require("./localDB/order.json");
 
 const port = process.env.PORT || 3000
 require('dotenv').config()
@@ -15,15 +17,14 @@ app.use(bodyParser.json())
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 let token = '';
-const localDB = []
 
 const KLARNA_API_ENDPOINT='https://api.playground.klarna.com'
 const KLARNA_USERNAME='PK37038_984cc1e6eebd'
 const KLARNA_PASSWORD='pRJJCMQitPSm4879'
 
-app.get('/payment/:order_id', (req, res) => {
-    if (localDB.length) {
-        const paymentData = localDB.find(order => order.order_id === req.params.order_id)
+app.get('/payment/:order_id', async (req, res) => {
+    if (orders.length) {
+        const paymentData = await orders.find(order => order.order_id === req.params.order_id)
         if (Object.keys(paymentData).length) {
             res.render('payment', { data: paymentData.html_snippet })
         } else {
@@ -59,13 +60,17 @@ app.post('/payment', (req, res) => {
         }
     })
         .then(function (response) {
-            localDB.push(response.data)
-            console.log("Data", response.data);
+            orders.push(response.data)
+            fs.writeFile("./localDB/order.json", JSON.stringify(orders), err => {
+                if (err) throw err; 
+               
+                console.log("Done writing"); // Success
+            });
             token = 'Basic' + response['request']['_header'].split("Basic")[1].split("==")[0] + '=='
             res.send({ status: "success", order_id: response.data.order_id })
         })
         .catch(function (error) {
-            res.send({ status: "error" })
+            res.send({ status: "error", error })
         });
 });
 
